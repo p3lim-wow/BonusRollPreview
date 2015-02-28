a54fe47ec89d19b2959b9a68e50655b50714cc79
Adrian L Lange <mail@p3lim.net>
2015-02-28 23:58:17 +0100

    Add global names to every frame
    This will aid people that wants to skin the addon

--- BonusRollPreview.lua
+++ BonusRollPreview.lua
@@ -35,7 +35,7 @@ local function HotspotEnter()
 			for index = 1, numSpecs do
 				local specID, name, _, texture = GetSpecializationInfo(index)
 
-				local SpecButton = CreateFrame('Button', nil, Buttons)
+				local SpecButton = CreateFrame('Button', '$parentSpecButton' .. index, Buttons)
 				SpecButton:SetPoint('LEFT', index * 28, 0)
 				SpecButton:SetSize(22, 22)
 				SpecButton:SetScript('OnClick', SpecButtonClick)
@@ -46,11 +46,11 @@ local function HotspotEnter()
 				SpecButton.specID = specID
 				SpecButton.name = name
 
-				local Icon = SpecButton:CreateTexture(nil, 'OVERLAY', nil, 1)
+				local Icon = SpecButton:CreateTexture('$parentIcon', 'OVERLAY', nil, 1)
 				Icon:SetAllPoints()
 				Icon:SetTexture(texture)
 
-				local Ring = SpecButton:CreateTexture(nil, 'OVERLAY', nil, 2)
+				local Ring = SpecButton:CreateTexture('$parentRing', 'OVERLAY', nil, 2)
 				Ring:SetPoint('TOPLEFT', -6, 6)
 				Ring:SetSize(58, 58)
 				Ring:SetTexture([[Interface\Minimap\Minimap-TrackingBorder]])
@@ -186,36 +186,36 @@ end
 local function GetItemLine(index)
 	local ItemButton = itemButtons[index]
 	if(not ItemButton) then
-		ItemButton = CreateFrame('Button', nil, Container.ScrollChild)
+		ItemButton = CreateFrame('Button', '$parentItemButton' .. index, Container.ScrollChild)
 		ItemButton:SetPoint('TOPLEFT', 6, (index - 1) * -40)
 		ItemButton:SetPoint('TOPRIGHT', -22, (index - 1) * -40)
 		ItemButton:SetHeight(38)
 
-		local Icon = ItemButton:CreateTexture(nil, 'BACKGROUND')
+		local Icon = ItemButton:CreateTexture('$parentIcon', 'BACKGROUND')
 		Icon:SetPoint('TOPLEFT', 1, -1)
 		Icon:SetSize(36, 36)
 		ItemButton.Icon = Icon
 
-		local Background = ItemButton:CreateTexture(nil, 'BORDER')
+		local Background = ItemButton:CreateTexture('$parentBackground', 'BORDER')
 		Background:SetAllPoints()
 		Background:SetTexture([[Interface\EncounterJournal\UI-EncounterJournalTextures]])
 		Background:SetTexCoord(0.00195313, 0.62890625, 0.61816406, 0.66210938)
 		Background:SetDesaturated(true)
 
-		local Name = ItemButton:CreateFontString(nil, 'ARTWORK', 'GameFontNormalMed3')
+		local Name = ItemButton:CreateFontString('$parentName', 'ARTWORK', 'GameFontNormalMed3')
 		Name:SetPoint('TOPLEFT', Icon, 'TOPRIGHT', 7, -4)
 		Name:SetPoint('TOPRIGHT', -6, -4)
 		Name:SetHeight(12)
 		Name:SetJustifyH('LEFT')
 		ItemButton.Name = Name
 
-		local Class = ItemButton:CreateFontString(nil, 'ARTWORK', 'GameFontHighlight')
+		local Class = ItemButton:CreateFontString('$parentClass', 'ARTWORK', 'GameFontHighlight')
 		Class:SetPoint('BOTTOMRIGHT', -6, 5)
 		Class:SetSize(0, 12)
 		Class:SetJustifyH('RIGHT')
 		ItemButton.Class = Class
 
-		local Slot = ItemButton:CreateFontString(nil, 'ARTWORK', 'GameFontHighlight')
+		local Slot = ItemButton:CreateFontString('$parentSlot', 'ARTWORK', 'GameFontHighlight')
 		Slot:SetPoint('BOTTOMLEFT', Icon, 'BOTTOMRIGHT', 7, 4)
 		Slot:SetPoint('BOTTOMRIGHT', Class, 'BOTTOMLEFT', -15, 0)
 		Slot:SetSize(0, 12)
@@ -360,11 +360,11 @@ function Container:PLAYER_LOGIN()
 		print('|cffff8080BonusRollPreview:|r You\'re running a conflicting addon, type /reload to resolve')
 	end
 
-	local ScrollChild = CreateFrame('Frame', nil, self)
+	local ScrollChild = CreateFrame('Frame', '$parentScrollChild', self)
 	ScrollChild:SetHeight(1) -- Completely ignores this value, bug?
 	self.ScrollChild = ScrollChild
 
-	local Scroll = CreateFrame('ScrollFrame', nil, self)
+	local Scroll = CreateFrame('ScrollFrame', '$parentScrollFrame', self)
 	Scroll:SetPoint('TOPLEFT', 0, -6)
 	Scroll:SetPoint('BOTTOMRIGHT', 0, 6)
 	Scroll:SetScrollChild(ScrollChild)
@@ -376,19 +376,20 @@ function Container:PLAYER_LOGIN()
 	self:SetBackdropBorderColor(2/3, 2/3, 2/3)
 	self:EnableMouseWheel(true)
 
-	local Slider = CreateFrame('Slider', nil, Scroll)
+	local Slider = CreateFrame('Slider', '$parentScrollBar', Scroll)
 	Slider:SetPoint('TOPRIGHT', -5, -16)
 	Slider:SetPoint('BOTTOMRIGHT', -5, 14)
 	Slider:SetWidth(16)
 	Slider:SetFrameLevel(self:GetFrameLevel() + 10)
-	Slider:SetThumbTexture([[Interface\Buttons\UI-ScrollBar-Knob]])
 	self.Slider = Slider
 
-	local Thumb = Slider:GetThumbTexture()
+	local Thumb = Scroll:CreateTexture('$parentThumbTexture')
 	Thumb:SetSize(16, 24)
+	Thumb:SetTexture([[Interface\Buttons\UI-ScrollBar-Knob]])
 	Thumb:SetTexCoord(1/4, 3/4, 1/8, 7/8)
+	Slider:SetThumbTexture(Thumb)
 
-	local Up = CreateFrame('Button', nil, Slider)
+	local Up = CreateFrame('Button', '$parentScrollUpButton', Slider)
 	Up:SetPoint('BOTTOM', Slider, 'TOP')
 	Up:SetSize(16, 16)
 	Up:SetNormalTexture([[Interface\Buttons\UI-ScrollBar-ScrollUpButton-Up]])
@@ -402,7 +403,7 @@ function Container:PLAYER_LOGIN()
 		Slider:SetValue(Slider:GetValue() - Slider:GetHeight() / 3)
 	end)
 
-	local Down = CreateFrame('Button', nil, Slider)
+	local Down = CreateFrame('Button', '$parentScrollDownButton', Slider)
 	Down:SetPoint('TOP', Slider, 'BOTTOM')
 	Down:SetSize(16, 16)
 	Down:SetScript('OnClick', ScrollClick)
@@ -454,44 +455,44 @@ function Container:PLAYER_LOGIN()
 	Handle:SetScript('OnClick', HandleClick)
 	Handle.Arrow = Handle:GetNormalTexture()
 
-	local HandleBackground = Handle:CreateTexture(nil, 'BACKGROUND')
+	local HandleBackground = Handle:CreateTexture('$parentBackground', 'BACKGROUND')
 	HandleBackground:SetAllPoints()
 	HandleBackground:SetTexture(0, 0, 0, 0.8)
 
-	local TopCenter = Handle:CreateTexture(nil, 'BORDER')
+	local TopCenter = Handle:CreateTexture('$parentTopCenter', 'BORDER')
 	TopCenter:SetPoint('TOP', 0, 4.5)
 	TopCenter:SetSize(24, 12)
 	TopCenter:SetTexture([[Interface\RaidFrame\RaidPanel-UpperMiddle]])
 	Handle.TopCenter = TopCenter
 
-	local TopRight = Handle:CreateTexture(nil, 'BORDER')
+	local TopRight = Handle:CreateTexture('$parentTopRight', 'BORDER')
 	TopRight:SetPoint('TOPRIGHT', 4, 4)
 	TopRight:SetSize(24, 20)
 	TopRight:SetTexture([[Interface\RaidFrame\RaidPanel-UpperRight]])
 	TopRight:SetTexCoord(0, 1, 0, 0.8)
 	Handle.TopRight = TopRight
 
-	local TopLeft = Handle:CreateTexture(nil, 'BORDER')
+	local TopLeft = Handle:CreateTexture('$parentTopLeft', 'BORDER')
 	TopLeft:SetPoint('TOPLEFT', -4, 4)
 	TopLeft:SetSize(24, 20)
 	TopLeft:SetTexture([[Interface\RaidFrame\RaidPanel-UpperLeft]])
 	TopLeft:SetTexCoord(0, 1, 0, 0.8)
 	Handle.TopLeft = TopLeft
 
-	local BottomCenter = Handle:CreateTexture(nil, 'BORDER')
+	local BottomCenter = Handle:CreateTexture('$parentBottomCenter', 'BORDER')
 	BottomCenter:SetPoint('BOTTOM', 0, -9)
 	BottomCenter:SetSize(24, 12)
 	BottomCenter:SetTexture([[Interface\RaidFrame\RaidPanel-BottomMiddle]])
 	Handle.BottomCenter = BottomCenter
 
-	local BottomRight = Handle:CreateTexture(nil, 'BORDER')
+	local BottomRight = Handle:CreateTexture('$parentBottomRight', 'BORDER')
 	BottomRight:SetPoint('BOTTOMRIGHT', 4, -6)
 	BottomRight:SetSize(24, 22)
 	BottomRight:SetTexture([[Interface\RaidFrame\RaidPanel-BottomRight]])
 	BottomRight:SetTexCoord(0, 1, 0.1, 1)
 	Handle.BottomRight = BottomRight
 
-	local BottomLeft = Handle:CreateTexture(nil, 'BORDER')
+	local BottomLeft = Handle:CreateTexture('$parentBottomLeft', 'BORDER')
 	BottomLeft:SetPoint('BOTTOMLEFT', -4, -6)
 	BottomLeft:SetSize(24, 22)
 	BottomLeft:SetTexture([[Interface\RaidFrame\RaidPanel-BottomLeft]])
