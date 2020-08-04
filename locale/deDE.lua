386ff9b73dd3aa85136fce4970d93f937d5c2d37
Adrian L Lange <mail@p3lim.net>
2020-08-04 20:30:53 +0200

    German translation courtesy of SunnySunflow @ CurseForge

--- locale/deDE.lua
+++ locale/deDE.lua
@@ -1,8 +1,8 @@
 local L = select(2, ...)('deDE')
 
 -- Config
-L['Lock/unlock movement with /brp lock, and reset the position with /brp reset.'] = 'Lock/unlock movement with /brp lock, and reset the position with /brp reset.' -- needs translation
-L['Always unfold the loot list'] = 'Always unfold the loot list' -- needs translation
-L['Direction the loot list should appear'] = 'Direction the loot list should appear' -- needs translation
-L['Up'] = 'Up' -- needs translation
-L['Down'] = 'Down' -- needs translation
+L['Lock/unlock movement with /brp lock, and reset the position with /brp reset.'] = 'Die Bewegung mit /brp lock sperren/entsperren und die Position mit /brp reset zuruecksetzen.'
+L['Always unfold the loot list'] = 'Entfalte immer die Beuteliste'
+L['Direction the loot list should appear'] = 'Die Richtung der Beuteliste soll erscheinen'
+L['Up'] = 'Oben'
+L['Down'] = 'Unten'
