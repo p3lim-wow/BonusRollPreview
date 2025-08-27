a8e74ec86e2cd059bea347084ec61ad7397a7255
Adrian L Lange <git@p3lim.net>
2025-08-27 16:48:53 +0200

    Re-indent

--- FavoriteProviders.lua
+++ FavoriteProviders.lua
@@ -1,96 +1,96 @@
 local addonName,addon = ...
 
 addon.favoriteProviders = {
-  AtlasLootClassic = false,
-  BastionLoot = false,
-  LOIHLoot = false,
-  LootAlarm = false,
-  LootReserve = false,
+	AtlasLootClassic = false,
+	BastionLoot = false,
+	LOIHLoot = false,
+	LootAlarm = false,
+	LootReserve = false,
 }
 
 local sortedFavProviders = { } -- only used for options menu & login check
 local haveFavoriteProviders
 function addon:GetAvailableFavoriteProviders()
-  sortedFavProviders = wipe(sortedFavProviders)
-  for name,status in pairs(addon.favoriteProviders) do
-    local _, _, _, loadable, reason, security, updateAvailable = C_AddOns.GetAddOnInfo(name)
-    local enabled = AddOnUtil.IsAddOnEnabledForCurrentCharacter(name)
-    if (not reason or reason == '') and loadable and enabled then
-      table.insert(sortedFavProviders,name)
-      addon.favoriteProviders[name] = true -- use this as a registry
-    else
-      addon.favoriteProviders[name] = false
-    end
-  end
-  haveFavoriteProviders = CountTable(sortedFavProviders) > 0
-  if haveFavoriteProviders then
-    table.sort(sortedFavProviders)
-  else
-    BonusRollPreviewDB.favoritesOnly = false
-  end
-  return haveFavoriteProviders, sortedFavProviders
+	sortedFavProviders = wipe(sortedFavProviders)
+	for name,status in pairs(addon.favoriteProviders) do
+		local _, _, _, loadable, reason, security, updateAvailable = C_AddOns.GetAddOnInfo(name)
+		local enabled = AddOnUtil.IsAddOnEnabledForCurrentCharacter(name)
+		if (not reason or reason == '') and loadable and enabled then
+			table.insert(sortedFavProviders,name)
+			addon.favoriteProviders[name] = true -- use this as a registry
+		else
+			addon.favoriteProviders[name] = false
+		end
+	end
+	haveFavoriteProviders = CountTable(sortedFavProviders) > 0
+	if haveFavoriteProviders then
+		table.sort(sortedFavProviders)
+	else
+		BonusRollPreviewDB.favoritesOnly = false
+	end
+	return haveFavoriteProviders, sortedFavProviders
 end
 
 local providerChecker = {
-  AtlasLootClassic = function(itemID)
-    if AtlasLoot and AtlasLoot.Addons and AtlasLoot.Addons.GetAddon then
-      local Favourites = AtlasLoot.Addons:GetAddon("Favourites")
-      if Favourites and Favourites:IsEnabled() and Favourites.IsFavouriteItemID then
-        if Favourites:IsFavouriteItemID(itemID) then
-          local icon = Favourites:GetIconForActiveItemID(itemID)
-          return "|cff00ff00AL|r"..(icon and format("|T%s:0|t",icon)) or ''
-        end
-      end
-    end
-  end,
-  BastionLoot = function(itemID)
-    if BastionLoot and BastionLoot.GetFavorite then
-      return BastionLoot:GetFavorite(itemID)
-    end
-  end,
-  LOIHLoot = function(itemID)
-    if LOIHLOOT_GLOBAL_PRIVATE and LOIHLOOT_GLOBAL_PRIVATE.IsItemWishList then
-      local isFav, list = LOIHLOOT_GLOBAL_PRIVATE:IsItemWishList(itemID)
-      if isFav then
-        return format("|cff006666%s|r",list)
-      end
-    end
-  end,
-  LootAlarm = function(itemID)
-    if LootAlarm and LootAlarm.IsInitialized and LootAlarm.GetActiveProfileName and LootAlarm.HasItemInActiveProfile then
-      if LootAlarm:IsInitialized() then
-        local profile = LootAlarm:GetActiveProfileName()
-        local isFav = LootAlarm:HasItemInActiveProfile(itemID)
-        return isFav and format("|cff2E8B57%s|r:%s","LA",profile)
-      end
-    end
-  end,
-  LootReserve = function(itemID)
-    if LootReserve and LootReserve.Client and LootReserve.Client.IsFavorite then
-      return LootReserve.Client:IsFavorite(itemID) and "|cffDDA0DDLR|r"
-    end
-  end,
+	AtlasLootClassic = function(itemID)
+		if AtlasLoot and AtlasLoot.Addons and AtlasLoot.Addons.GetAddon then
+			local Favourites = AtlasLoot.Addons:GetAddon("Favourites")
+			if Favourites and Favourites:IsEnabled() and Favourites.IsFavouriteItemID then
+				if Favourites:IsFavouriteItemID(itemID) then
+					local icon = Favourites:GetIconForActiveItemID(itemID)
+					return "|cff00ff00AL|r"..(icon and format("|T%s:0|t",icon)) or ''
+				end
+			end
+		end
+	end,
+	BastionLoot = function(itemID)
+		if BastionLoot and BastionLoot.GetFavorite then
+			return BastionLoot:GetFavorite(itemID)
+		end
+	end,
+	LOIHLoot = function(itemID)
+		if LOIHLOOT_GLOBAL_PRIVATE and LOIHLOOT_GLOBAL_PRIVATE.IsItemWishList then
+			local isFav, list = LOIHLOOT_GLOBAL_PRIVATE:IsItemWishList(itemID)
+			if isFav then
+				return format("|cff006666%s|r",list)
+			end
+		end
+	end,
+	LootAlarm = function(itemID)
+		if LootAlarm and LootAlarm.IsInitialized and LootAlarm.GetActiveProfileName and LootAlarm.HasItemInActiveProfile then
+			if LootAlarm:IsInitialized() then
+				local profile = LootAlarm:GetActiveProfileName()
+				local isFav = LootAlarm:HasItemInActiveProfile(itemID)
+				return isFav and format("|cff2E8B57%s|r:%s","LA",profile)
+			end
+		end
+	end,
+	LootReserve = function(itemID)
+		if LootReserve and LootReserve.Client and LootReserve.Client.IsFavorite then
+			return LootReserve.Client:IsFavorite(itemID) and "|cffDDA0DDLR|r"
+		end
+	end,
 }
 
 function addon:IsFavoritedItem(itemID)
-  local favoriteProvider = BonusRollPreviewDB.favoriteProvider
-  if not favoriteProvider then return end
-  if favoriteProvider == 'ANY' then
-    -- just iterate our registry
-    local isFav
-    for provider, status in pairs(addon.favoriteProviders) do
-      if status and providerChecker[provider] then
-        isFav = providerChecker[provider](itemID)
-        if isFav then
-          return isFav
-        end
-      end
-    end
-  else
-    -- just the configured checker
-    -- check we haven't ended up with a dangling reference
-    if providerChecker[favoriteProvider] and addon.favoriteProviders[favoriteProvider] then
-      return providerChecker[favoriteProvider](itemID)
-    end
-  end
+	local favoriteProvider = BonusRollPreviewDB.favoriteProvider
+	if not favoriteProvider then return end
+	if favoriteProvider == 'ANY' then
+		-- just iterate our registry
+		local isFav
+		for provider, status in pairs(addon.favoriteProviders) do
+			if status and providerChecker[provider] then
+				isFav = providerChecker[provider](itemID)
+				if isFav then
+					return isFav
+				end
+			end
+		end
+	else
+		-- just the configured checker
+		-- check we haven't ended up with a dangling reference
+		if providerChecker[favoriteProvider] and addon.favoriteProviders[favoriteProvider] then
+			return providerChecker[favoriteProvider](itemID)
+		end
+	end
 end
--- Options.lua
+++ Options.lua
@@ -2,94 +2,94 @@ local addonName, addon = ...
 local L = addon.L
 
 local settings = {
-  {
-    key = 'alwaysShow',
-    type = 'toggle',
-    title = L['Auto unfold'],
-    tooltip = L['Always unfold the loot list'],
-    default = false
-  },
-  {
-    key = 'fillDirection',
-    type = 'menu',
-    title = L['Direction'],
-    tooltip = L['Direction the loot list should appear'],
-    default = 'UP',
-    options = {
-      {value = 'UP', label = L['Up']},
-      {value = 'DOWN', label = L['Down']},
-    },
-  },
+	{
+		key = 'alwaysShow',
+		type = 'toggle',
+		title = L['Auto unfold'],
+		tooltip = L['Always unfold the loot list'],
+		default = false
+	},
+	{
+		key = 'fillDirection',
+		type = 'menu',
+		title = L['Direction'],
+		tooltip = L['Direction the loot list should appear'],
+		default = 'UP',
+		options = {
+			{value = 'UP', label = L['Up']},
+			{value = 'DOWN', label = L['Down']},
+		},
+	},
 }
 
 if addon.GetAvailableFavoriteProviders then
-  local haveFavoriteProviders, sortedFavProviders = addon:GetAvailableFavoriteProviders()
-  do
-    if haveFavoriteProviders then
-      tinsert(settings, {
-        key = 'favoriteProvider',
-        type = 'menu',
-        title = L['Favorites from'],
-        tooltip = L['Select a Favorite Items provider'],
-        default = 'ANY',
-        options = {
-          {value = 'ANY', label = L['Any']},
-        },
-      })
-      tinsert(settings,{
-        key = 'favoriteAlert',
-        type = 'toggle',
-        title = L['Favorite alert'],
-        tooltip = L['Visual and Audio cue for favorited Items'],
-        default = true,
-        parent = 'favoriteProvider',
-      })
-      tinsert(settings,{
-        key = 'favoritesOnly',
-        type = 'toggle',
-        title = L['Only favorites'],
-        tooltip = L['Filter preview to favorited items only'],
-        default = false,
-        parent = 'favoriteProvider',
-      })
-      for index,setting in ipairs(settings) do
-        if setting.key == 'favoriteProvider' then
-          if setting.type == 'menu' and setting.options then
-            for order,provider in ipairs(sortedFavProviders) do
-              tinsert(setting.options, {value = provider, label = provider})
-            end
-          end
-          break
-        end
-      end
-    end
-  end
+	local haveFavoriteProviders, sortedFavProviders = addon:GetAvailableFavoriteProviders()
+	do
+		if haveFavoriteProviders then
+			tinsert(settings, {
+				key = 'favoriteProvider',
+				type = 'menu',
+				title = L['Favorites from'],
+				tooltip = L['Select a Favorite Items provider'],
+				default = 'ANY',
+				options = {
+					{value = 'ANY', label = L['Any']},
+				},
+			})
+			tinsert(settings,{
+				key = 'favoriteAlert',
+				type = 'toggle',
+				title = L['Favorite alert'],
+				tooltip = L['Visual and Audio cue for favorited Items'],
+				default = true,
+				parent = 'favoriteProvider',
+			})
+			tinsert(settings,{
+				key = 'favoritesOnly',
+				type = 'toggle',
+				title = L['Only favorites'],
+				tooltip = L['Filter preview to favorited items only'],
+				default = false,
+				parent = 'favoriteProvider',
+			})
+			for index,setting in ipairs(settings) do
+				if setting.key == 'favoriteProvider' then
+					if setting.type == 'menu' and setting.options then
+						for order,provider in ipairs(sortedFavProviders) do
+							tinsert(setting.options, {value = provider, label = provider})
+						end
+					end
+					break
+				end
+			end
+		end
+	end
 end
 
 addon:RegisterSettings('BonusRollPreviewDB', settings)
 
 function addon:OnLogin()
-  if not BonusRollPreviewDB.anchor then
-    BonusRollPreviewDB.anchor = {'CENTER', 'UIParent', 'CENTER', 0, 0}
-  end
-  if addon.GetAvailableFavoriteProviders then
-    if not haveFavoriteProviders then
-      BonusRollPreviewDB.favoritesOnly = false
-    elseif not addon.favoriteProviders[BonusRollPreviewDB.favoriteProvider] then
-      BonusRollPreviewDB.favoriteProvider = 'ANY'
-    end
-  end
+	if not BonusRollPreviewDB.anchor then
+		BonusRollPreviewDB.anchor = {'CENTER', 'UIParent', 'CENTER', 0, 0}
+	end
+	if addon.GetAvailableFavoriteProviders then
+		if not haveFavoriteProviders then
+			BonusRollPreviewDB.favoritesOnly = false
+		elseif not addon.favoriteProviders[BonusRollPreviewDB.favoriteProvider] then
+			BonusRollPreviewDB.favoriteProvider = 'ANY'
+		end
+	end
 end
 
 addon:RegisterSlash('/bonusrollpreview', '/brp', function(msg)
-  msg = msg:lower()
+	msg = msg:lower()
 
-  if(msg == 'unlock' or msg == 'lock') then
-    BonusRollPreview:ToggleLock()
-  elseif(msg == 'reset') then
-    BonusRollPreviewDB.anchor = {'CENTER', 'UIParent', 'CENTER', 0, 0}
-    BonusRollPreview:OnEvent('PLAYER_LOGIN')
-  else
-    addon:OpenSettings()
-  end
+	if(msg == 'unlock' or msg == 'lock') then
+		BonusRollPreview:ToggleLock()
+	elseif(msg == 'reset') then
+		BonusRollPreviewDB.anchor = {'CENTER', 'UIParent', 'CENTER', 0, 0}
+		BonusRollPreview:OnEvent('PLAYER_LOGIN')
+	else
+		addon:OpenSettings()
+	end
 end)
